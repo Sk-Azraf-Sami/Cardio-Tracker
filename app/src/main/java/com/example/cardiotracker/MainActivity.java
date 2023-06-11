adb970f91bcd865beb8f1cc52db55664b6d6016f
Sk-Azraf-Sami <azraf7026official@gmail.com>
2023-06-12 02:02:18 +0600

    Logout button is now functional

--- app/src/main/java/com/example/cardiotracker/MainActivity.java
+++ app/src/main/java/com/example/cardiotracker/MainActivity.java
@@ -2,13 +2,33 @@ package com.example.cardiotracker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button btnLogout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnLogout = findViewById(R.id.btnLogout); // Replace with your logout button ID
+
+        // Set click listener for the logout button
+        btnLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Create an intent to navigate to the LoginActivity
+                Intent intent = new Intent(MainActivity.this, Login.class);
+                // Clear the back stack, so the user cannot navigate back to the current activity
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
+                // Start the LoginActivity and finish the current activity
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
