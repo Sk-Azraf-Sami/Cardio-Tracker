ed9df3c0d3ff67314487d65bf682e52cc36615d5
Sk-Azraf-Sami <azraf7026official@gmail.com>
2023-06-24 18:14:37 +0600

    Update Login UI Test

--- app/src/androidTest/java/com/example/cardiotracker/LoginUITest.java
+++ app/src/androidTest/java/com/example/cardiotracker/LoginUITest.java
@@ -1,78 +1,123 @@
 package com.example.cardiotracker;
 
 import androidx.test.espresso.Espresso;
+import androidx.test.espresso.IdlingRegistry;
 import androidx.test.espresso.action.ViewActions;
-import androidx.test.espresso.assertion.ViewAssertions;
-import androidx.test.espresso.matcher.RootMatchers;
-import androidx.test.espresso.matcher.ViewMatchers;
-import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.espresso.idling.CountingIdlingResource;
+import androidx.test.espresso.intent.rule.IntentsTestRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.filters.LargeTest;
 
-import com.example.cardiotracker.R;
-import com.example.cardiotracker.Login;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
-import org.hamcrest.Matchers;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.intent.Intents.intended;
+import static androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+
+//import com.example.cardiotracker.ui.MainActivity;
+//import com.example.cardiotracker.ui.auth.SignUp;
+import com.example.cardiotracker.R;
+
 @RunWith(AndroidJUnit4.class)
+@LargeTest
 public class LoginUITest {
 
     @Rule
-    public ActivityScenarioRule<Login> activityScenarioRule = new ActivityScenarioRule<>(Login.class);
+    public IntentsTestRule<Login> activityRule = new IntentsTestRule<>(Login.class);
 
-    @Test
-    public void loginSuccessTest() {
-        String email = "test@example.com";
-        String password = "password123";
+    private CountingIdlingResource countingIdlingResource;
 
-        Espresso.onView(ViewMatchers.withId(R.id.email))
-                .perform(ViewActions.typeText(email), ViewActions.closeSoftKeyboard());
+    @Before
+    public void setUp() {
+        countingIdlingResource = new CountingIdlingResource("FirebaseAuthentication");
+        IdlingRegistry.getInstance().register(countingIdlingResource);
+    }
 
-        Espresso.onView(ViewMatchers.withId(R.id.password))
-                .perform(ViewActions.typeText(password), ViewActions.closeSoftKeyboard());
-
-        Espresso.onView(ViewMatchers.withId(R.id.login))
-                .perform(ViewActions.click());
-
-        // Add assertions based on the actual behavior of your app after successful login
-        // For example, you can check if a toast message is displayed:
-        Espresso.onView(ViewMatchers.withText("Successful Login"))
-                .inRoot(RootMatchers.withDecorView(Matchers.not(ViewMatchers.withId(android.R.id.content))))
-                .check(ViewAssertions.matches(ViewMatchers.isDisplayed()));
+    @After
+    public void tearDown() {
+        IdlingRegistry.getInstance().unregister(countingIdlingResource);
     }
 
     @Test
-    public void loginEmptyFieldsTest() {
-        Espresso.onView(ViewMatchers.withId(R.id.login))
-                .perform(ViewActions.click());
+    public void loginSuccess() {
 
-        // Add assertions based on the actual behavior of your app when login fields are empty
-        // For example, you can check if a toast message is displayed:
-        Espresso.onView(ViewMatchers.withText("Enter Email"))
-                .inRoot(RootMatchers.withDecorView(Matchers.not(ViewMatchers.withId(android.R.id.content))))
-                .check(ViewAssertions.matches(ViewMatchers.isDisplayed()));
+        String test_email = "check@gmail.com";
+        String test_password = "kacchi";
+        // Perform login with valid credentials
+        Espresso.onView(withId(R.id.emailEditText)).perform(ViewActions.typeText(test_email));
+        Espresso.onView(withId(R.id.passwordEditText)).perform(ViewActions.typeText(test_password));
+
+        Espresso.closeSoftKeyboard();
+        Espresso.onView(withId(R.id.loginButton)).perform(ViewActions.click());
+
+        // Increment the CountingIdlingResource before starting the Firebase authentication task
+        countingIdlingResource.increment();
+
+        // Wait for Firebase authentication to complete
+        FirebaseAuth.getInstance().signInWithEmailAndPassword(test_email, test_password)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(Task<AuthResult> task) {
+                        // Decrement the CountingIdlingResource after the Firebase authentication task is completed
+                        countingIdlingResource.decrement();
+                    }
+                });
+
+        // Verify that MainActivity is launched
+        intended(hasComponent(MainActivity.class.getName()));
     }
 
     @Test
-    public void loginAuthenticationFailedTest() {
-        String email = "invalid@example.com";
-        String password = "password123";
+    public void loginFailure() {
 
-        Espresso.onView(ViewMatchers.withId(R.id.email))
-                .perform(ViewActions.typeText(email), ViewActions.closeSoftKeyboard());
+        String test_email = "invalid@gmail.com";
+        String test_password = "wrongpassword";
+        // Perform login with invalid credentials
+        Espresso.onView(withId(R.id.emailEditText)).perform(ViewActions.typeText(test_email));
+        Espresso.onView(withId(R.id.passwordEditText)).perform(ViewActions.typeText(test_password));
+        Espresso.closeSoftKeyboard();
+        Espresso.onView(withId(R.id.loginButton)).perform(ViewActions.click());
 
-        Espresso.onView(ViewMatchers.withId(R.id.password))
-                .perform(ViewActions.typeText(password), ViewActions.closeSoftKeyboard());
+        // Wait for Firebase authentication to complete
+        countingIdlingResource.increment();
 
-        Espresso.onView(ViewMatchers.withId(R.id.login))
-                .perform(ViewActions.click());
+        // Verify that the login failed (Stay on the Login activity)
+        FirebaseAuth.getInstance().signInWithEmailAndPassword(test_email, test_password)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(Task<AuthResult> task) {
+                        // Decrement the CountingIdlingResource after the Firebase authentication task is completed
+                        countingIdlingResource.decrement();
+                        if (!task.isSuccessful()) {
+                            // Authentication failed
+                            Espresso.onView(withText("Authentication failed"))
+                                    .check(matches(isDisplayed()));
+                        }
+                    }
+                });
+    }
 
-        // Add assertions based on the actual behavior of your app when login authentication fails
-        // For example, you can check if a toast message is displayed:
-        Espresso.onView(ViewMatchers.withText("Authentication failed"))
-                .inRoot(RootMatchers.withDecorView(Matchers.not(ViewMatchers.withId(android.R.id.content))))
-                .check(ViewAssertions.matches(ViewMatchers.isDisplayed()));
+
+
+    @Test
+    public void signUpLinkClick() {
+        // Perform click on the "Create Account" link
+        Espresso.onView(withId(R.id.createAccountTextView)).perform(ViewActions.click());
+
+        // Verify that SignUp activity is launched
+        intended(hasComponent(SignUp.class.getName()));
     }
 }
