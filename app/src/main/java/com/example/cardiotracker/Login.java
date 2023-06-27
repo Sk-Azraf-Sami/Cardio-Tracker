cdc0828e79fa3bf898bbdff533ca951d6eec218e
Wasif <wzf8801@gmail.com>
2023-06-27 18:10:35 +0600

    wasif

--- app/src/main/java/com/example/cardiotracker/Login.java
+++ app/src/main/java/com/example/cardiotracker/Login.java
@@ -12,14 +12,11 @@ import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-
-import com.example.cardiotracker.MainActivity;
-import com.example.cardiotracker.SignUp;
-
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class Login extends AppCompatActivity {
 
