505ece4d55e652bb5b81000ed5c33144254449aa
Wasif <wzf8801@gmail.com>
2023-06-27 18:04:26 +0600

    solved

--- app/src/main/java/com/example/cardiotracker/Login.java
+++ app/src/main/java/com/example/cardiotracker/Login.java
@@ -1,8 +1,5 @@
 package com.example.cardiotracker;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
@@ -12,12 +9,14 @@ import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 
 public class Login extends AppCompatActivity {
 
@@ -28,7 +27,17 @@ public class Login extends AppCompatActivity {
     ProgressBar progressBar;
     TextView textView;
 
+ /*   @Override
+    public void onStart() {
+        super.onStart();
 
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser != null){
+            Intent intent=new Intent(getApplicationContext(),MainActivity.class);
+            startActivity(intent);
+            finish();
+        }
+    }*/
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
