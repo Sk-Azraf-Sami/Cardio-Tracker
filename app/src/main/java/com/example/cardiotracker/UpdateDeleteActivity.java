ab8fc5b8b94f27f0974607941edf5a2441c1da26
Sk-Azraf-Sami <azraf7026official@gmail.com>
2023-06-28 16:49:53 +0600

    show record of list in text fields

--- app/src/main/java/com/example/cardiotracker/UpdateDeleteActivity.java
+++ app/src/main/java/com/example/cardiotracker/UpdateDeleteActivity.java
@@ -1,5 +1,6 @@
 package com.example.cardiotracker;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -8,15 +9,18 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.material.dialog.MaterialAlertDialogBuilder;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class UpdateDeleteActivity extends AppCompatActivity {
 
-    private EditText commentEditText, diaPressureEditText, heartRateEditText, sysPressureEditText,
-            systemDateEditText, systemTimeEditText;
+    private EditText commentEditText, diaPressureEditText, heartRateEditText, sysPressureEditText;
     private Button updateButton, deleteButton;
     private DatabaseReference databaseReference;
     private FirebaseAuth firebaseAuth;
@@ -38,8 +42,6 @@ public class UpdateDeleteActivity extends AppCompatActivity {
         diaPressureEditText = findViewById(R.id.diaPressureEditText);
         heartRateEditText = findViewById(R.id.heartRateEditText);
         sysPressureEditText = findViewById(R.id.sysPressureEditText);
-       // systemDateEditText = findViewById(R.id.systemDateEditText);
-       // systemTimeEditText = findViewById(R.id.systemTimeEditText);
         updateButton = findViewById(R.id.updateButton);
         deleteButton = findViewById(R.id.deleteButton);
 
@@ -47,6 +49,30 @@ public class UpdateDeleteActivity extends AppCompatActivity {
         if (extras != null) {
             key = extras.getString("key");
         }
+
+        databaseReference.child(key).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if (dataSnapshot.exists()) {
+                    String comment = dataSnapshot.child("Comment").getValue(String.class);
+                    String diaPressure = dataSnapshot.child("DiaPressure").getValue(String.class);
+                    String heartRate = dataSnapshot.child("HeartRate").getValue(String.class);
+                    String sysPressure = dataSnapshot.child("SysPressure").getValue(String.class);
+
+                    // Populate the EditText fields with the retrieved data
+                    commentEditText.setText(comment != null ? comment : "");
+                    diaPressureEditText.setText(diaPressure != null ? diaPressure : "");
+                    heartRateEditText.setText(heartRate != null ? heartRate : "");
+                    sysPressureEditText.setText(sysPressure != null ? sysPressure : "");
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                // Handle the error if retrieval is unsuccessful
+                Toast.makeText(UpdateDeleteActivity.this, "Failed to retrieve data", Toast.LENGTH_SHORT).show();
+            }
+        });
 
         updateButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -58,7 +84,7 @@ public class UpdateDeleteActivity extends AppCompatActivity {
         deleteButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                deleteData();
+                showConfirmationDialog();
             }
         });
     }
@@ -68,18 +94,30 @@ public class UpdateDeleteActivity extends AppCompatActivity {
         String diaPressure = diaPressureEditText.getText().toString().trim();
         String heartRate = heartRateEditText.getText().toString().trim();
         String sysPressure = sysPressureEditText.getText().toString().trim();
-        //String systemDate = systemDateEditText.getText().toString().trim();
-        //String systemTime = systemTimeEditText.getText().toString().trim();
+
+        if (comment.isEmpty() || diaPressure.isEmpty() || heartRate.isEmpty() || sysPressure.isEmpty()) {
+            Toast.makeText(this, "Please fill in all the fields", Toast.LENGTH_SHORT).show();
+            return;
+        }
 
         // Update the data fields in the database using the key
         databaseReference.child(key).child("Comment").setValue(comment);
         databaseReference.child(key).child("DiaPressure").setValue(diaPressure);
         databaseReference.child(key).child("HeartRate").setValue(heartRate);
         databaseReference.child(key).child("SysPressure").setValue(sysPressure);
-        //databaseReference.child(key).child("SystemDate").setValue(systemDate);
-        //databaseReference.child(key).child("SystemTime").setValue(systemTime);
 
         Toast.makeText(this, "Data updated successfully!", Toast.LENGTH_SHORT).show();
+    }
+
+    private void showConfirmationDialog() {
+        new MaterialAlertDialogBuilder(this)
+                .setTitle("Confirmation")
+                .setMessage("Are you sure you want to delete this data?")
+                .setPositiveButton("Delete", (dialog, which) -> {
+                    deleteData();
+                })
+                .setNegativeButton("Cancel", null)
+                .show();
     }
 
     private void deleteData() {
