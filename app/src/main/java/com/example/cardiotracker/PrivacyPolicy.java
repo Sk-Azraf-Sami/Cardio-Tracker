c15b74aec34613c93a869a503daefe5d8126a15d
Wasif <wzf8801@gmail.com>
2023-07-05 21:11:58 +0600

    javadoc 9,10,11

--- app/src/main/java/com/example/cardiotracker/PrivacyPolicy.java
+++ app/src/main/java/com/example/cardiotracker/PrivacyPolicy.java
@@ -1,11 +1,22 @@
 package com.example.cardiotracker;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+/**
+ * This class represents the activity that displays the privacy policy of the application.
+ * It provides information about the privacy practices and policies followed by the application.
+ * The activity shows the privacy policy content to the user.
+ */
 public class PrivacyPolicy extends AppCompatActivity {
-
+    /**
+     * Called when the activity is starting. This is where most initialization of the activity should go.
+     * Sets the content view to display the privacy policy layout.
+     *
+     * @param savedInstanceState If the activity is being re-initialized after previously being shut down,
+     *                           then this Bundle contains the data it most recently supplied in onSaveInstanceState(Bundle).
+     *                           Otherwise, it is null.
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
